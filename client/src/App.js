053fae3444c52f1133c148ddc2aaf450e6ef9192
Dayton Kotas <Kotasdm@gmatc.matc.edu>
2023-10-18 23:12:24 -0500

    Completed Activity 7

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-template-curly-in-string */
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 class App extends React.Component{
   state = {
@@ -23,12 +25,33 @@ class App extends React.Component{
 
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-        GoodThings
-        </header>
-        {this.state.data}
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1> GoodThings </h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </header>
+          <main>
+            <Route exact path="/">
+          {this.state.data}
+            </Route>
+            <Switch>
+              <Route exact path="/register" Component={Register} />
+              <Route exact path="/login" Component={Login} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 
