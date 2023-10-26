e88cb000fe12cf4e5e368ab7bc7709f03d3d4496
Dayton Kotas <Kotasdm@gmatc.matc.edu>
2023-10-25 22:36:32 -0500

    Completed Activity 8

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,127 @@
 /* eslint-disable no-template-curly-in-string */
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React from 'react';
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    data:null
+    data: null,
+  };
+
+  state = {
+    data: null,
+    token: null,
+    user: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000")
+      .then((response) => {
+        this.setState({
+          data: response.data,
+        });
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      });
+
+    this.authenticateUser();
   }
 
   componentDidMount() {
-    axios.get('http://localhost:5000')
+    axios
+      .get("http://localhost:5000")
       .then((response) => {
         this.setState({
-          data: response.data
-        })
+          data: response.data,
+        });
       })
       .catch((error) => {
-        console.error('Error fetching data: ${error}');
-      })
+        console.error("Error fetching data: ${error}");
+      });
   }
+
+  logOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({ user: null, token: null});
+  }
+
+  authenticateUser = () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      localStorage.removeItem("user");
+      this.setState({ user: null });
+    }
+
+    if (token) {
+      const config = {
+        headers: {
+          "x-auth-token": token,
+        },
+      };
+
+      axios
+        .get("http://localhost:5000/api/auth", config)
+        .then((response) => {
+          localStorage.setItem("user", response.data.name);
+          this.setState({ user: response.data.name });
+        })
+        .catch((error) => {
+          localStorage.removeItem("user");
+          this.setState({ user: null });
+          console.error("Error logging in: ${error}");
+        });
+    }
+  };
 
   render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-        GoodThings
-        </header>
-        {this.state.data}
-      </div>
-    );
+  let { user, data } = this.state;
+  const authProps = {
+    authenticateUser: this.authenticateUser
   }
-
 }
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        GoodThings 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
+<header className="App-header">
+  <h1>GoodThings</h1>
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/register">Register</Link>
+    </li>
+    <li>
+      {user ?
+        <Link to="" onClick={this.logout}>Log out</Link> :
+        <Link to="/login">Log in</Link>
+      }
+    </li>
+  </ul>
+</header>
+
+<main>
+  <Route exact path="/">
+    {user ?
+      <React.Fragment>
+        <div>Hello {user}!</div>
+        <div>{data}</div>
+      </React.Fragment> :
+      <React.Fragment>
+        Please Register or Login
+      </React.Fragment>
+    }
+  </Route>
+  <Switch>
+    <Route
+      exact path="/register"
+      render={() => <Register {...authProps} />} />
+    <Route
+      exact path="/login"
+      render={() => <Login {...authProps} />} />
+  </Switch>
+</main>
 
 export default App;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
+import auth from './middleware/auth'
 
 // Initialize express application
 const app = express();
@@ -53,64 +54,110 @@ app.post(
         } else {
             const { name, email, password } = req.body;
             try {
-                // Check if user exists
-                let user = await User.findOne({ email: email });
-                if (user) {
-                    return res
-                    .status(400)
-                    .json({ errors: [{ msg: 'User already exists'}] });
-                }
+              // Check if user exists
+              let user = await User.findOne({ email: email });
+              if (user) {
+                return res
+                  .status(400)
+                  .json({ errors: [{ msg: "User already exists" }] });
+              }
 
-                // Create a new user
-                user = new User({
-                    name: name,
-                    email: email,
-                    password: password
-                });
+              // Create a new user
+              user = new User({
+                name: name,
+                email: email,
+                password: password,
+              });
 
-                // Encrypt the password 
-                const salt = await bcrypt.genSalt(10);
-                user.password = await bcrypt.hash(password, salt);
+              // Encrypt the password
+              const salt = await bcrypt.genSalt(10);
+              user.password = await bcrypt.hash(password, salt);
 
-                // Save the db and return
-                await user.save();
+              // Save the db and return
+              await user.save();
 
-                // Generate and return a JWT token
-                const payload = {
-                    user: {
-                        id: user.id
-                    }
-                };
-
-                jwt.sign(
-                    payload,
-                    config.get('jwtSecret'),
-                    { expiresIn: '10hr' },
-                    (err, token) => {
-                        if (err) throw err;
-                        res.json({ token: token });
-                    }
-                );
+              // Generate and return a JWT token
+              returnToken(user, res);
             } catch (error) {
-                res.status(500).send('Server error');
+              res.status(500).send("Server error");
             }
         }
     }
 );
 
+/**
+ * @route GET api/auth
+ * @desc Authenticate user
+ */
+app.get('/api/auth', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send('Unknown server error');
+  }
+});
+
+/**
+ * @route POST api/login
+ * @desc Login user
+ */
+app.post(
+  '/api/login',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'A password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    } else {
+      const { email, password } = req.body;
+      try {
+        // Check if user exists
+        let user = await User.findOne({ email: email });
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid email or password' }] });
+        }
+
+        // Check password
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid email or password' }] });
+        }
+
+        // Generate and return a JWT token
+        returnToken(user, res);
+      } catch (error) {
+        res.status(500).send('Server error');
+      }
+    }
+  }
+);
+
+const returnToken = (user, res) => {
+  const payload = {
+    user: {
+      id: user.id,
+    },
+  };
+
+  jwt.sign(
+    payload,
+    config.get("jwtSecret"),
+    { expiresIn: "10hr" },
+    (err, token) => {
+      if (err) throw err;
+      res.json({ token: token });
+    }
+  );
+};
+
 // Connection Listener 
 const port = 5000;
 app.listen(port, () => console.log('Express server running on port ${port}'));
-
-//app.listen(3000, () => console.log('Express server running on port 3000'));
-
-/*
-
-    (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() });
-        } else {
-            return res.send(req.body);
-        }
-    }*/
